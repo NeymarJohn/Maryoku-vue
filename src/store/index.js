8e589b51f4943695b12c169c6790ed550dfb874d
Sunriver <seriousman1994217winter@gmail.com>
2019-02-06 15:31:45 +0200

    extended authentication token and start of multi-tenancy

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,6 @@ export default new Vuex.Store({
     componentsList: null,
     vendorsList: null,
     occasionsArray: null,
-    currenciesArray: null,
     calendarId: null,
   },
   mutations: {
