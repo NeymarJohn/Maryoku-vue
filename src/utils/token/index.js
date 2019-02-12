d0b3c33349f119c12bc49beb5f100b73b19d4678
Sunriver <seriousman1994217winter@gmail.com>
2019-02-12 13:39:54 +0200

    Geography - remove, Remove event name field - put it to the top, Prefill occasion - and allow to change it

--- src/utils/token/index.js
+++
@@ -1,8 +0,0 @@
-const TOKEN ="user_token"
-
-
-export const setToken=(token)=>localStorage.setItem(TOKEN,token)
-
-export const getToken=()=>localStorage.getItem(TOKEN)
-
-export const deleteToken=()=>localStorage.removeItem(TOKEN)
