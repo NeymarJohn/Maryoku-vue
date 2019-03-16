94c0318a7cca1d564e617c20f7a0a53b3ce52b6d
Sunriver <seriousman1994217winter@gmail.com>
2019-03-17 01:32:15 +0200

    create My Events and Event details components

---
+++ src/pages/app/Profile/EventDetails.vue
@@ -0,0 +1,239 @@
+<template>
+    <div>
+      <div class="md-layout">
+            <div class="md-layout-item md-size-15">
+                <label>
+                     Where
+                 </label>
+            </div>
+            <div class="md-layout-item md-size-85" style="padding-left: 0px; ">
+                <div>
+                    <md-icon style="font-size: 15px !important; padding-right: 1%;">room</md-icon>
+                </div>
+            </div>
+        </div>
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-size-100">
+                <p>The main office @ 15 Sunset Blvd. Transportation will be provided from the office. If you're arriving solo, the venue is located @ street name. Parking is available in building garage.
+                </p>
+            </div>
+        </div>
+    
+    
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-size-15">
+                <label>
+                    when
+                 </label>
+            </div>
+            <div class="md-layout-item md-size-85" style="padding-left: 0px;">
+                <div>
+                    <md-icon style="font-size: 15px !important;padding-right: 2%;">alarm</md-icon>
+                </div>
+            </div>
+        </div>
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-size-100">
+                <p>
+                    March 06, 2019, Wednesday - 1:00 PM
+                </p>
+            </div>
+        </div>
+    
+    
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-size-27" style="padding-right: 0px;">
+                <label>
+                    Dress code
+                </label>
+            </div>
+            <div class="md-layout-item md-size-70" style="padding-left: 0px; margin-right: 1%;">
+                <div>
+                    <md-icon style="font-size: 15px !important;padding-right: 35px;">face</md-icon>
+                </div>
+            </div>
+        </div>
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-size-100">
+                <p>
+                    Corporate Style
+                </p>
+            </div>
+        </div>
+    
+
+        <div class="md-layout " style="margin-bottom:8%; margin-top:2%">
+            <div class="md-layout-item md-size-70">
+                <div class="md-layout">
+                    <div class="md-layout-item md-size-35" style="padding-left: 0px;">
+                        <div style="width:50%; height: 100%;">
+                            <button class="comming-button" ><span class="comming">Comming</span></button>
+                        </div>
+                    </div>
+                    <div class="md-layout-item md-size-35" style="margin-left: 0px;padding-left: 0px;">
+                        <div style="width:50%; height: 100%;">
+                         <button class="notcomming-button" > <span class="notcomming">Not coming</span></button>
+                    </div>
+                    </div>
+                </div>    
+            </div>
+             <div class="md-layout-item">
+                    
+            </div>
+             <div class="md-layout-item">
+               
+            </div>
+        </div>
+       
+    
+        <div class="md-layout">
+            <div class="md-layout-item md-gutter ">
+                <div style="width:100%;height:100%">
+                    <img src="https://www.w3schools.com/w3css/img_lights.jpg" title="">
+    
+                </div>
+            </div>
+            <div class="md-layout-item " style="padding-left: 0px;">
+                <div>
+                    <img src="https://images.pexels.com/photos/237018/pexels-photo-237018.jpeg?cs=srgb&dl=asphalt-autumn-beauty-237018.jpg&fm=jpg" title="">
+                </div>
+            </div>
+            <div class="md-layout-item " style="padding-left: 0px;">
+                
+                <div class="md-layout md-gutter md-alignment-center-center" style="background-color: #e1dfdf; width: 100%; height: 100%; margin:0 auto;">
+                    <div class="md-layout-item" style="text-align:center;">
+                        <div class="border-add-photo">
+                            <div class="md-layout md-gutter md-alignment-center-center" style="background-color: #e1dfdf; width: 100%; height: 100%; text-align:center;margin:auto;">  
+                               <div class="md-layout-item" style="text-align:center;">
+                                   <div style="text-align:center;">
+                                     <md-icon class="add-photo-icon" style="color:#ffffff;">add</md-icon>
+                               </div>
+                               
+                               </div>
+                            </div>
+                        </div>
+                            <div class="md-layout md-gutter md-alignment-center-center" style="margin-left: 0px;margin-right: 0px;">
+                                 <div class="md-layout-item" style="padding-left: 0px;padding-right: 0px;">
+                                   <div class="add-photo">add photo</div>
+                                </div>
+                            </div>
+                    </div>
+                
+                   
+                </div>
+                  
+            </div>
+        </div>
+    
+    
+    
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            EventLocation: {
+                type: String
+            },
+            EventDate: {
+                type: String
+            },
+            EventDressCode: {
+                type: String
+            }
+        },
+        mounted() {
+            console.log('EEEe', this.EventLocation)
+        },
+    }
+</script>
+
+<style lang="scss">
+    .test {
+        font-size: 20px;
+    }
+    
+    .add-photo {
+        width: 67px;
+        height: 14px;
+        font-family: Roboto;
+        font-size: 14px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        text-align: center;
+        color: #ffffff;
+        margin-left: 10px;
+
+    }
+
+    .add-photo-icon{
+        font-size: 15px !important; 
+        margin:auto; 
+        padding-right: 20px;
+        
+    }
+    .border-add-photo{
+        border-style: dotted;
+        border-width: 2px;
+         border-color: #ffffff;
+         text-align: center;
+         margin: auto;
+         width: 35px;
+         height: 35px;
+    }
+
+    .comming-button
+    {
+
+     width: 94px;
+     height: 26px;
+     border-radius: 5px;
+     background-color: #fb8f05;
+    }
+
+    .notcomming-button{
+         width: 94px;
+        height: 26px;
+        border-radius: 5px;
+        background-color: #e1dfdf;
+
+    }
+
+    .comming{
+        width: 48px;
+        height: 15px;
+        font-family: Roboto;
+        font-size: 14px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        text-align: left;
+        color: #ffffff;
+    }
+
+    .notcomming{
+        width: 72px;
+        height: 15px;
+        font-family: Roboto;
+        font-size: 14px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        text-align: left;
+        color: #ffffff;
+    }
+</style>
---
+++ src/pages/app/Profile/MyEvents.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="my-event">
+        <md-card >
+            <md-card-header class="md-card-header-icon ">
+               <div class="md-layout">
+                    <div class="card-icon  header-icon-myEvents">
+                        <md-icon style=" text-align: center;">perm_identity</md-icon>
+                    </div>
+    
+                    <div class="md-layout-item  title-My-Events">
+                        My Events
+                    </div>
+                  
+                </div>
+            </md-card-header>
+            <md-card-content>
+                <collapse
+            :collapse="[
+              'Event 1',
+              'Event 2',
+              'Event 3',
+              'Event 4',
+              'Event 5',
+              'Event 6',
+              'Event 7']"
+            icon="query_builder"
+            color-collapse="warning">
+              <template slot="md-collapse-pane-1">
+                <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+              <template slot="md-collapse-pane-2">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+              <template slot="md-collapse-pane-3">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+               <template slot="md-collapse-pane-4">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+               <template slot="md-collapse-pane-5">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+               <template slot="md-collapse-pane-6">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+               <template slot="md-collapse-pane-7">
+                  <event-details :where="EventLocation" :when="EventDate" :DressCode="EventDressCode"></event-details>
+              </template>
+        </collapse>
+            </md-card-content>
+    
+        </md-card>
+    </div>
+</template>
+<script>
+import { Collapse } from "@/components";
+import EventDetails from "./EventDetails"
+export default {
+    components: {
+    Collapse,
+    EventDetails
+  },
+  data()
+  {
+      return { 
+          EventLocation:'',
+          EventDate:'',
+          EventDressCode:''
+      }
+  },
+  mounted()
+  {
+      this.EventLocation = 'test'; 
+      this.EventDate='20-02-2019';
+      this.EventDressCode = 'test22';
+  }
+}
+</script>
+
+<style lang="scss">
+    .title-My-Events {
+        width: 81px;
+        height: 18px;
+        font-family: Roboto;
+        font-size: 18px;
+        font-weight: 300;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        text-align: left;
+        color: var(--slate);
+        padding-top: 20px;
+    }
+    
+    .header-icon-myEvents {
+        width: 63px;
+        height: 64px;
+        border-radius: 3px;
+        box-shadow: 0px 4px 20px 0 rgba(0, 0, 0, 0.14);
+        background-image:  linear-gradient(to right, #ffa625, #fb8d02),linear-gradient(to top, #000000, #ffffff);
+    }
+
+    .my-event{
+        width: 513px;
+        height: 842px;
+        border-radius: 6px;
+        // box-shadow: 0px 1px 4px 0 rgba(0, 0, 0, 0.14);
+        // background-color: #ffffff;
+        overflow-y:auto; 
+        overflow-x: hidden;
+        padding: 0px;
+        margin:0px;
+    }
+</style>
