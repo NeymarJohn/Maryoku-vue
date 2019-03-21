a4de61519a45035a042b420803d4e632d715427d
Sunriver <seriousman1994217winter@gmail.com>
2019-03-21 20:48:54 +0200

    create HolidayCelebrate Component

---
+++ src/pages/app/Profile/HolidaysCelebrate.vue
@@ -0,0 +1,40 @@
+<template>
+    <div style="width:100%">
+        <md-card>
+            <md-card-header class="md-card-header-icon md-card-header-green">
+                <div class="card-icon" style="padding: 12px;">
+                <md-icon>ballot</md-icon>
+                </div>
+                <h4 class="title">Holidays I Celebrate</h4>
+            </md-card-header>
+            <md-card-content>    
+                <div class="md-layout">
+                    <div class="md-layout-item">
+                        <md-checkbox v-model="holidayType" value="Christian">Christian</md-checkbox>
+                    </div>
+                    <div class="md-layout-item">
+                        <md-checkbox v-model="holidayType" value="Jewish">Jewish</md-checkbox>
+                    </div>
+                </div>
+
+                <div class="md-layout">
+                    <div class="md-layout-item">
+                        <md-checkbox v-model="holidayType" value="Hindu" >Hindu</md-checkbox>
+                    </div>
+                    <div class="md-layout-item">
+                        <md-checkbox  v-model="holidayType" value="Muslim">Muslim</md-checkbox>
+                    </div>
+                </div>
+            </md-card-content>    
+        </md-card>        
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            holidayType:''
+        }
+    }
+}
+</script>
