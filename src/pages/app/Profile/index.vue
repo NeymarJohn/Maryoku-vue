9967b36e7d8027903d97eabc7865b2c8febec7bd
Sunriver <seriousman1994217winter@gmail.com>
2019-03-21 20:51:37 +0200

    reorder Cards on Profile Pages

--- src/pages/app/Profile/index.vue
+++ src/pages/app/Profile/index.vue
@@ -4,16 +4,28 @@
     <vue-element-loading :active="isLoading" spinner="ring" color="#FF547C" is-full-screen />
 
     <div class="md-layout ">
-      <div class="md-layout-item md-size-33">
+      <div class="md-layout-item md-size-30">
         <personal-information :userInfo="auth.user"></personal-information>
       </div>
-      <div class="md-layout-item md-size-33" >
+      <div class="md-layout-item md-size-30" >
+          <div class="md-layout">
+            <dietary-constraints></dietary-constraints>
+          </div>
+       
+          <div class="md-layout">
 
-        <my-events :events="upCommingEvents"></my-events>
+              <my-special-dates :birthDate="auth.user.me.birthday" :workingSince="auth.user.me.companyStartDate"></my-special-dates>
+          </div>
+
+          <div class="md-layout">
+              <holidays-celebrate></holidays-celebrate>
+          </div>
       </div>
 
-      <div class="md-layout-item md-size-33">
-        <dietary-constraints></dietary-constraints>
+      <div class="md-layout-item md-size-35">
+        
+
+        <my-events :events="upCommingEvents"></my-events>
       </div>
 
     </div>
@@ -26,6 +38,7 @@
   import MyEvents from "./MyEvents.vue";
   import DietaryConstraints from "./DietaryConstraints.vue";
   import MySpecialDates from "./MySpecialDates.vue";
+  import HolidaysCelebrate from './HolidaysCelebrate.vue';
   import auth from '@/auth';
   import { mapGetters } from 'vuex';
   // import {
@@ -44,6 +57,7 @@
       MyEvents,
       DietaryConstraints,
       MySpecialDates,
+      HolidaysCelebrate
 
     },
     data() {
