c844061a7f9d098babe401194eafc1012a43b7ec
Sunriver <seriousman1994217winter@gmail.com>
2019-04-30 07:18:14 +0300

    fix add event dialog dropdowns

--- src/pages/app/Events/CurrentEvents.vue
+++ src/pages/app/Events/CurrentEvents.vue
@@ -259,6 +259,10 @@
       this.getEvent();
       if (this.components.length === 0) {
         this.$store.dispatch("event/getComponents");
+        this.$store.dispatch("event/getCategories", this.auth.user.defaultCalendarId);
+        this.$store.dispatch("event/getEventTypes", this.auth.user.defaultCalendarId);
+        this.$store.dispatch("event/getCurrencies");
+        this.$store.dispatch("event/getEventThemes");
       }
     },
     methods: {
