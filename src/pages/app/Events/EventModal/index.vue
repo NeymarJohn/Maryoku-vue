cc3fbf8b65e20cabadf465d9110750fed56b0aa2
Sunriver <seriousman1994217winter@gmail.com>
2019-05-19 03:10:33 +0300

    implement adding vendors to event blocks

--- src/pages/app/Events/EventModal/index.vue
+++ src/pages/app/Events/EventModal/index.vue
@@ -33,7 +33,7 @@
                             <div class="md-layout-item md-small-size-100">
                               <md-autocomplete v-model="occasion"
                                                 data-vv-name="occasion"
-                                                :md-options="occasionsList"
+                                                :md-options="occasionList"
                                                 @md-opened="mdOpened"
                                                 class="change-icon-order select-with-icon mb16"
                                                 :class="[{'md-error': errors.has('occasion')}]">
@@ -200,7 +200,8 @@
   import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
   import CalendarEvent from '@/models/CalendarEvent';
   import {Modal} from "@/components";
-  import Calendar from "@/models/Calendar"
+  import Calendar from "@/models/Calendar";
+  import Occasion from "@/models/Occasion";
   import swal from "sweetalert2";
   import { error } from 'util';
   import moment from 'moment';
@@ -256,6 +257,7 @@
           required: true,
         },
       },
+        occasionList : []
     }),
 
     created() {
@@ -407,6 +409,18 @@
       this.$root.$on('submitForm', () => {
         this.validateEvent();
       });
+
+        let _calendar = new Calendar({id: this.auth.user.defaultCalendarId});
+
+
+        new Occasion().for(_calendar).get()
+            .then(resp =>{
+                this.occasionList = resp.map((val) => val.title);
+            })
+            .catch(error =>{
+                console.log('error =>> ', error);
+            });
+
     },
     methods: {
       ...mapMutations('EventPlannerVuex', ['resetForm', 'setEventModal', 'setEventProperty']),
