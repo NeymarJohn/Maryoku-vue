20c6fd34e10de2c83902a8fdf8a34a3dee11ed88
Sunriver <seriousman1994217winter@gmail.com>
2019-06-10 21:16:10 +0300

    Changing the score get two confirmation messages

--- src/models/EventInvitee.js
+++ src/models/EventInvitee.js
@@ -1,14 +1,9 @@
 import Model from './Model';
 
 export default class EventInvitee extends Model {
-    resource() {
-        return 'invitees'
-    }
-
-    static saveInvitees(ctx, resource){
-        const key = `event.${event.id}.invitees`;
-        ctx.$ls.set(key,resource, Model.DEFAULT_EXPIRATION_MILLIS);
-    }
+  resource() {
+    return 'invitees'
+  }
 
     static fetch(ctx, calendar, event, force){
         return new Promise ((resolve, reject)=> {
