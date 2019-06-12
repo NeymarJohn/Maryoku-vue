36056dcb7f767329fc09f8777824fec06e657f08
Sunriver <seriousman1994217winter@gmail.com>
2019-06-12 19:52:22 +0300

    https://trello.com/c/wjPm0Ct9/24-imagepng

--- src/pages/app/Vendors/Vendors.vue
+++ src/pages/app/Vendors/Vendors.vue
@@ -1,202 +1,213 @@
 <template>
-  <div class="md-layout">
-    <div class="md-layout-item md-size-50">
-      <md-card>
-        <md-card-header class="md-card-header-text md-card-header-warning">
+    <div class="md-layout">
+        <div class="md-layout-item md-size-50">
+            <md-card>
+                <md-card-header class="md-card-header-text md-card-header-warning">
 
-          <div class="card-text">
-            <h4 class="title" style="color: white;">Vendors Pool</h4>
-            <div class="ct-label">See all vendors uploaded below</div>
-          </div>
+                    <div class="card-text">
+                        <h4 class="title" style="color: white;">Vendors Pool</h4>
+                        <div class="ct-label">See all vendors uploaded below</div>
+                    </div>
 
-          <div class="table table-stats vendors-actions-list">
-            <md-button name="vendors-add-vendor" class="vendors-add-vendor md-info" @click="openInviteModal">
-              Add Vendor
-            </md-button>
-            <md-button name="vendors-upload-vendors" @click="openUploadModal" class="vendors-upload-vendors md-info">
-              Upload Excel File
-            </md-button>
-          </div>
+                    <div class="table table-stats vendors-actions-list">
+                        <md-button name="vendors-add-vendor" class="vendors-add-vendor md-info" @click="openInviteModal">
+                            Add Vendor
+                        </md-button>
+                        <md-button name="vendors-upload-vendors" @click="openUploadModal" class="vendors-upload-vendors md-info">
+                            Upload Excel File
+                        </md-button>
+                    </div>
 
-        </md-card-header>
-        <md-card-content style="min-height: 60px;">
-          <vue-element-loading :active="loadingData" spinner="ring" color="#FF547C"/>
+                </md-card-header>
+                <md-card-content style="min-height: 60px;">
+                    <vue-element-loading :active="loadingData" spinner="ring" color="#FF547C"/>
 
-          <vendors-table
-                  v-if="vendorsList"
-                  :tooltipModels="tooltipModels"
-                  @select-vendor="onSelectVendor"
-                  @close-vendor="onCloseVendorForm"
-                  :vendorsList="vendorsList"
-                  mode="listing"
-                  ref="VendorsTable">
+                    <vendors-table
+                        v-if="vendorsList"
+                        :tooltipModels="tooltipModels"
+                        @select-vendor="onSelectVendor"
+                        @close-vendor="onCloseVendorForm"
+                        :vendorsList="vendorsList"
+                        mode="listing"
+                        ref="VendorsTable">
 
-          </vendors-table>
-          <md-card-actions md-alignment="space-between" v-if="pagination.limit < pagination.total">
-            <div class="">
-              <p class="card-category">Showing {{ pagination.from }} to {{ pagination.limit < pagination.total ? pagination.limit : pagination.total }} of {{ pagination.total }} records</p>
-            </div>
-            <pagination class="pagination-no-border pagination-info"
-                        @input="pageChanged($event)"
-                        v-model="pagination.page"
-                        :per-page="pagination.limit"
-                        :total="pagination.total">
-            </pagination>
-          </md-card-actions>
+                    </vendors-table>
+                    <md-card-actions md-alignment="space-between" v-if="pagination.limit < pagination.total">
+                        <div class="">
+                            <p class="card-category">Showing {{ pagination.from }} to {{ pagination.limit < pagination.total ? pagination.limit : pagination.total }} of {{ pagination.total }} records</p>
+                        </div>
+                        <pagination class="pagination-no-border pagination-info"
+                                    @input="pageChanged($event)"
+                                    v-model="pagination.page"
+                                    :per-page="pagination.limit"
+                                    :total="pagination.total">
+                        </pagination>
+                    </md-card-actions>
 
-        </md-card-content>
-      </md-card>
-    </div>
+                </md-card-content>
+            </md-card>
+        </div>
 
-    <div class="md-layout-item md-size-50" v-if="vendor_selected || add_vendor">
-      <company-form :selected_vendor="selected_vendor" :creation_mode="add_vendor" @vendorCreated="fetchData(1)"></company-form>
-    </div>
+        <div class="md-layout-item md-size-50" v-if="vendor_selected || add_vendor">
+            <company-form :categories.sync="buildingBlocksList" :selected_vendor="selected_vendor" :creation_mode="add_vendor" @vendorCreated="fetchData(1)"></company-form>
+        </div>
 
 
-    <create-modal @vendorCreated="fetchData(1)"  ref="inviteModal"></create-modal>
-    <upload-modal @vendorImported="fetchData(1)"  ref="uploadModal"></upload-modal>
+        <create-modal @vendorCreated="fetchData(1)"  ref="inviteModal"></create-modal>
+        <upload-modal @vendorImported="fetchData(1)"  ref="uploadModal"></upload-modal>
     </div>
 </template>
 
 <script>
-  import CreateModal from './CreateModal';
-  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
-  import VendorsTable from './Table/vendorsList';
-  import companyForm from './Form/companyForm.vue';
-  import UploadModal from './ImportVendors';
-  import { Pagination } from "@/components"
-  import Vendors from "@/models/Vendors";
-  import VendorCategories from "@/models/VendorCategories";
-  import VueElementLoading from 'vue-element-loading';
-  // import auth from '@/auth';
-  import { paginationMixin } from '@/mixins/pagination'
+    import CreateModal from './CreateModal';
+    import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
+    import VendorsTable from './Table/vendorsList';
+    import companyForm from './Form/companyForm.vue';
+    import UploadModal from './ImportVendors';
+    import { Pagination } from "@/components"
+    import Vendors from "@/models/Vendors";
+    import EventComponent from "@/models/EventComponent";
+    import VendorCategories from "@/models/VendorCategories";
+    import VueElementLoading from 'vue-element-loading';
+    // import auth from '@/auth';
+    import { paginationMixin } from '@/mixins/pagination'
 
 
-  export default {
-    components: {
-      CreateModal,
-      'vendors-table': VendorsTable,
-      VueElementLoading,
-      UploadModal,
-      Pagination,
-      companyForm
-    },
-    mixins: [paginationMixin],
-    data() {
-      return {
-        // auth: auth,
-        vendorsList: [],
-        tooltipModels: [],
-        loadingData: true,
-        importClicked: false,
-        tableHidden: true,
-        selected_vendor : {},
-        add_vendor : false,
-        vendor_selected : false
-      }
-    },
-    created() {
-      this.$auth.currentUser(this, true, function(){
-        this.fetchData(0);
-      }.bind(this));
-    },
-    methods: {
-      ...mapMutations('vendors', ['resetForm']),
+    export default {
+        components: {
+            CreateModal,
+            'vendors-table': VendorsTable,
+            VueElementLoading,
+            UploadModal,
+            Pagination,
+            companyForm
+        },
+        mixins: [paginationMixin],
+        data() {
+            return {
+                // auth: auth,
+                vendorsList: [],
+                tooltipModels: [],
+                loadingData: true,
+                importClicked: false,
+                tableHidden: true,
+                selected_vendor : {},
+                add_vendor : false,
+                vendor_selected : false,
+                buildingBlocksList: []
+            }
+        },
+        created() {
+            this.$auth.currentUser(this, true, function(){
+                this.fetchData(0);
+            }.bind(this));
+        },
+        methods: {
+            ...mapMutations('vendors', ['resetForm']),
 
-      fetchData(page) {
-        this.loadingData = true;
+            fetchData(page) {
+                this.loadingData = true;
 
+                new EventComponent().get().then(res=>{
+                    let list = [];
+                    res.forEach((parentBuildingBlock)=>{
+                        parentBuildingBlock.childComponents.forEach((bb)=>{
+                            list.push({id: bb.id, value: bb.title});
+                        });
+                    });
+                    this.buildingBlocksList = list;
+                });
 
-        Vendors.page(page)
-          .limit(this.pagination.limit)
-          .get().then(vendors => {
+                Vendors.page(page)
+                    .limit(this.pagination.limit)
+                    .get().then(vendors => {
 
-              console.log('vendors => ', vendors);
+                    console.log('vendors => ', vendors);
 
-          this.vendorsList = vendors[0].results;
+                    this.vendorsList = vendors[0].results;
 
-          this.updatePagination(vendors[0].model)
-          this.loadingData = false;
-
-
-            this.vendorsList.map((item, index) => {
-            this.tooltipModels.push({
-              value: false,
-              textarea: '',
-              rankingParameters: [
-                {
-                  name: 'Overal Experience',
-                  parameterName: 'overal_experience',
-                  value: ''
-                },
-                {
-                  name: 'Cleanliness and Maintenance',
-                  parameterName: 'cleanliness_and_maintenance',
-                  value: ''
-
-                },
-                {
-                  name: 'Accuracy',
-                  parameterName: 'accuracy',
-                  value: ''
-
-                },
-                {
-                  name: 'Value for money',
-                  parameterName: 'value_for_money',
-                  value: ''
-
-                }, {
-                  name: 'Service',
-                  parameterName: 'service',
-                  value: ''
-
-                },
-                {
-                  name: 'Location & Parking',
-                  parameterName: 'location_parking',
-                  value: ''
-
-                },
+                    this.updatePagination(vendors[0].model)
+                    this.loadingData = false;
 
 
-              ],
+                    this.vendorsList.map((item, index) => {
+                        this.tooltipModels.push({
+                            value: false,
+                            textarea: '',
+                            rankingParameters: [
+                                {
+                                    name: 'Overal Experience',
+                                    parameterName: 'overal_experience',
+                                    value: ''
+                                },
+                                {
+                                    name: 'Cleanliness and Maintenance',
+                                    parameterName: 'cleanliness_and_maintenance',
+                                    value: ''
 
-            })
-          });
-        }, (error) => {
-          console.log(error)
-        });
-      },
-      openInviteModal(){
+                                },
+                                {
+                                    name: 'Accuracy',
+                                    parameterName: 'accuracy',
+                                    value: ''
+
+                                },
+                                {
+                                    name: 'Value for money',
+                                    parameterName: 'value_for_money',
+                                    value: ''
+
+                                }, {
+                                    name: 'Service',
+                                    parameterName: 'service',
+                                    value: ''
+
+                                },
+                                {
+                                    name: 'Location & Parking',
+                                    parameterName: 'location_parking',
+                                    value: ''
+
+                                },
+
+
+                            ],
+
+                        })
+                    });
+                }, (error) => {
+                    console.log(error)
+                });
+            },
+            openInviteModal(){
 //        this.$refs.inviteModal.toggleModal(true);
 //        this.resetForm();
-          this.$set(this,'vendor_selected',false);
-          this.$set(this,'selected_vendor',{});
-          this.$set(this,'add_vendor',true);
-          this.$refs.VendorsTable.resetSelectedVendor({});
+                this.$set(this,'vendor_selected',false);
+                this.$set(this,'selected_vendor',{});
+                this.$set(this,'add_vendor',true);
+                this.$refs.VendorsTable.resetSelectedVendor({});
 
-      },
-      openUploadModal(){
-        this.$refs.uploadModal.toggleModal(true);
-      }
-      ,onSelectVendor(data) {
-          this.$set(this,'vendor_selected',true);
-            this.$set(this,'selected_vendor',data);
-            this.$set(this,'add_vendor',false);
+            },
+            openUploadModal(){
+                this.$refs.uploadModal.toggleModal(true);
+            }
+            ,onSelectVendor(data) {
+                this.$set(this,'vendor_selected',true);
+                this.$set(this,'selected_vendor',data);
+                this.$set(this,'add_vendor',false);
 
-        },
-        onCloseVendorForm(data){
-            this.$set(this,'vendor_selected',false);
-            this.$set(this,'selected_vendor',{});
-            this.$set(this,'add_vendor',false);
+            },
+            onCloseVendorForm(data){
+                this.$set(this,'vendor_selected',false);
+                this.$set(this,'selected_vendor',{});
+                this.$set(this,'add_vendor',false);
 
+            }
         }
-      }
-  };
+    };
 </script>
 <style lang="scss" scoped>
-  .md-table-head-label {
-    font-weight: 500;
-  }
+    .md-table-head-label {
+        font-weight: 500;
+    }
 </style>
