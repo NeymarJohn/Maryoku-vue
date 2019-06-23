c192006c77bc4190e8567f313f5ec8f940520ea4
Sunriver <seriousman1994217winter@gmail.com>
2019-06-23 06:49:41 +0300

    fixes

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  SERVER_URL: '"https://api.262days.com"'
+    NODE_ENV: '"production"',
+    SERVER_URL: '"https://api.262days.com"',
+    GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  SERVER_URL: '"https://api-dev.262days.com"'
+    NODE_ENV: '"testing"',
+    SERVER_URL: '"https://api-dev.262days.com"',
+    GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8'
 })
