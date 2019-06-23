662343fdb7f05c0170bee0684fee89a32bc39b47
Sunriver <seriousman1994217winter@gmail.com>
2019-06-23 07:00:20 +0300

    fixes

--- config/test.env.js
+++ config/test.env.js
@@ -5,5 +5,5 @@ const devEnv = require('./dev.env')
 module.exports = merge(devEnv, {
     NODE_ENV: '"testing"',
     SERVER_URL: '"https://api-dev.262days.com"',
-    GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8'
+    GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8"'
 })
