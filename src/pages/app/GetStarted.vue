fb04a8ee4c2384d8522c47892052d58e5d98dcb5
Sunriver <seriousman1994217winter@gmail.com>
2019-08-01 20:38:21 +0300

    vendors

--- src/pages/app/GetStarted.vue
+++ src/pages/app/GetStarted.vue
@@ -43,50 +43,53 @@
 </template>
 <script>
 
-    import EventSidePanel from '@/pages/app/Events/EventSidePanel';
+  import EventSidePanel from '@/pages/app/Events/EventSidePanel';
 
-    export default {
-        data() {
+  export default {
+    data() {
 
-        },
-        mounted(){
-            this.$auth.currentUser(this, true, () =>{
+    },
+    mounted(){
+      this.$auth.currentUser(this, true, () =>{
+        this.$root.$on("get-started-event-created", (eventId)=>{
+          this.$router.push({name: 'EditEvent', params: {id: eventId}});
+        });
+      });
+    },
+    methods: {
+      openEventModal() {
+        /*this.$router.push({ path: `/events` , name  : 'Events', params : { mode : 'create-event'} });
+        this.setEventModal({ showModal: true });
+        this.setModalSubmitTitle("Save");
+        this.setEditMode({ editMode: false });*/
 
-            });
-        },
-        methods: {
-            openEventModal() {
-                /*this.$router.push({ path: `/events` , name  : 'Events', params : { mode : 'create-event'} });
-                this.setEventModal({ showModal: true });
-                this.setModalSubmitTitle("Save");
-                this.setEditMode({ editMode: false });*/
+        let now = new Date();
+        window.currentPanel = this.$showPanel({
+          component: EventSidePanel,
+          cssClass: 'md-layout-item md-size-40 transition36 ',
+          openOn: 'right',
+          disableBgClick: false,
+          props: {
+            modalSubmitTitle: 'Save',
+            editMode: false,
+            sourceEventData: {eventStartMillis: new Date().getTime(), numberOfParticipants: this.$auth.user.customer.numberOfEmployees},
+            refreshEvents: null,
+            year: now.getFullYear(),
+            month: now.getMonth(),
+            occasionsOptions: this.occasionsArray,
+            openInPlannerOption: false
+          }
+        });
 
-                let now = new Date();
-                window.currentPanel = this.$showPanel({
-                    component: EventSidePanel,
-                    cssClass: 'md-layout-item md-size-40 transition36 ',
-                    openOn: 'right',
-                    disableBgClick: false,
-                    props: {
-                        modalSubmitTitle: 'Save',
-                        editMode: false,
-                        sourceEventData: {eventStartMillis: new Date().getTime(), numberOfParticipants: this.$auth.user.customer.numberOfEmployees},
-                        refreshEvents: null,
-                        year: now.getFullYear(),
-                        month: now.getMonth(),
-                        occasionsOptions: this.occasionsArray,
-                        openInPlannerOption: false
-                    }
-                });
-            },
-            openYearPlanner() {
-                this.$router.push({ path: `/planner`});
-            },
-            openVendorsUpload() {
-                this.$router.push({ path: `/vendors`});
-            }
-        }
+      },
+      openYearPlanner() {
+        this.$router.push({ path: `/planner`});
+      },
+      openVendorsUpload() {
+        this.$router.push({ path: `/vendors`});
+      }
     }
+  }
 </script>
 <style lang="scss" scoped>
 
