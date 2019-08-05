59ef8b043d043a620a76e45576eed58e8f85ae32
Sunriver <seriousman1994217winter@gmail.com>
2019-08-05 18:34:44 +0300

    vendors

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,16 @@
 'use strict'
+
+var webpack = require('webpack')
+
 module.exports = {
     NODE_ENV: '"production"',
     SERVER_URL: '"https://api.262days.com"',
     GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8"',
-    mode: 'production'
+
+    plugins: [
+        // ...
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
 }
--- config/test.env.js
+++ config/test.env.js
@@ -1,10 +1,17 @@
 'use strict'
 const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
+var webpack = require('webpack')
 
 module.exports = merge(devEnv, {
     NODE_ENV: '"testing"',
     SERVER_URL: '"https://api-dev.262days.com"',
     GOOGLE_API_KEY:'"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8"',
-    mode: 'production'
+
+    plugins: [
+        // ...
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
 })
