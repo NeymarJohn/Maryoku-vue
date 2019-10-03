9413935b35cf88fb7fcd61d65d858610ff9ab8c7
Sunriver <seriousman1994217winter@gmail.com>
2019-10-04 00:24:23 +0300

    make all elements except image list in vendor detail

--- src/pages/app/Vendors/CurrentVendors.vue
+++ src/pages/app/Vendors/CurrentVendors.vue
@@ -39,13 +39,21 @@
     </div>
     <div class="md-layout bg-white">
       <div class="md-layout-item">
+        <div class="banner-description">
+          <div class="banner-description-title">
+            <md-icon>view_list</md-icon>
+            <h4>
+              Description
+            </h4>
+          </div>
+          <div class="banner-description-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
         <div class="description">
-          <h4>
-            <md-icon>view_list</md-icon> Description
-          </h4>
-          <p>
-            Lorem
-          </p>
+          
         </div>
         <div class="text-group">
           <div class="">
@@ -79,7 +87,7 @@
           <div v-if="currentTab == 1">
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>apartment</md-icon>
                 <h4>
                   Capacity
                 </h4>
@@ -118,27 +126,138 @@
             </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>check_circle</md-icon>
                 <h4>
                   Services Offered by This Vendor
                 </h4>
               </div>
               <div class="tab-item-content-body">
+                <ul class="check-list">
+                  <li>
+                    <md-icon>restaurant</md-icon> <strong>Food</strong>
+                  </li>
+                  <li>
+                    <br/>
+                  </li>
+                  <li class="normal">
+                    <md-icon>check</md-icon> Catering via venue
+                  </li>
+                  <li class="normal">
+                    <md-icon>check</md-icon> Own food allowed
+                  </li>
+                  <li class="normal">
+                    <md-icon>check</md-icon> Alchol license
+                  </li>
+                  <li class="normal">
+                    <md-icon>check</md-icon> own beverages allowed
+                  </li>
+                  <li class="normal">
+                    <md-icon>check</md-icon> Meeting Catering
+                  </li>
+                  <li class="disabled">
+                    <md-icon></md-icon> <span>Dinnerware</span>
+                  </li>
+                </ul>
+                <div class="notes">
+                  <div class="notes-title">
+                    <h4>
+                      Attachment
+                    </h4>
+                  </div>
+                  <div class="notes-body">
+                    <div class="note-item">
+                      <md-icon>picture_as_pdf</md-icon> Food menu Option 1
+                    </div>
+                    <div class="note-item">
+                      <md-icon>picture_as_pdf</md-icon> Food menu Option 2
+                    </div>
+                    <div class="note-item">
+                      <md-icon>picture_as_pdf</md-icon> Food menu Option Insurance Certificate
+                    </div>
+                    <div class="note-item">
+                      <md-icon>picture_as_pdf</md-icon> Food menu Other Business Indication
+                    </div>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div class="divider">
             </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>menu_book</md-icon>
                 <h4>
                   Pricing and Rules
                 </h4>
               </div>
               <div class="tab-item-content-body">
+                <div class="text-vertical">
+                  <h5>
+                    $41
+                  </h5>
+                  <span>
+                    Price / person
+                  </span>
+                </div>
+                <div class="text-vertical">
+                  <h5>
+                    $74
+                  </h5>
+                  <span>
+                    Price / hour
+                  </span>
+                </div>
+                <div class="text-vertical">
+                  <h5>
+                    $25
+                  </h5>
+                  <span>
+                    Daliy rent
+                  </span>
+                </div>
+                <div class="text-vertical">
+                  <h5>
+                    $78
+                  </h5>
+                  <span>
+                    Minimum spend
+                  </span>
+                </div>
+                <div class="text-vertical">
+                  <h5>
+                    $50
+                  </h5>
+                  <span>
+                    Reservation fee
+                  </span>
+                </div>
+                <div class="text-vertical">
+                  <h5>
+                    $12
+                  </h5>
+                  <span>
+                    Cleaning fee
+                  </span>
+                </div>
+                <div class="notes">
+                  <div class="notes-title">
+                    <h4>
+                      Notes
+                    </h4>
+                  </div>
+                  <div class="notes-body">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div class="divider">
             </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>dehaze</md-icon>
                 <h4>
                   Similar Proposals Made by Vendor
                 </h4>
@@ -146,9 +265,11 @@
               <div class="tab-item-content-body">
               </div>
             </div>
+            <div class="divider">
+            </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>edit</md-icon>
                 <h4>
                   Feedbacks
                 </h4>
@@ -156,29 +277,41 @@
               <div class="tab-item-content-body">
               </div>
             </div>
+            <div class="divider">
+            </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>credit_card</md-icon>
                 <h4>
                   Payment Terms
                 </h4>
               </div>
               <div class="tab-item-content-body">
+                <p>
+                  A 50% deposit will be due on or before 18/1/20. The remaining balance will b collected a week prior to the event. We accept all major credit cards and checks.
+                </p>
               </div>
+            </div>
+            <div class="divider">
             </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>warning</md-icon>
                 <h4>
                   Cancellations Policy - Strict
                 </h4>
               </div>
               <div class="tab-item-content-body">
+                <p>
+                  Be due on or before 18/1/20. The remaining balance will be collected a week prior to the vent. We accept... <a class="more">Read more</a>
+                </p>
               </div>
+            </div>
+            <div class="divider">
             </div>
             <div class="tab-item-content">
               <div class="tab-item-content-title">
-                <md-icon>timeline</md-icon>
+                <md-icon>flip_to_back</md-icon>
                 <h4>
                   Similar Vendors
                 </h4>
@@ -262,6 +395,21 @@
 <style lang="scss" scoped>
   @import "@/assets/scss/md/_variables.scss";
   @import "@/assets/scss/md/_colors.scss";
+  .banner-description {
+    padding: 0 1em;
+    .banner-description-title {
+      display: flex;
+      i {
+        display: block;
+        margin-right: .5em;
+        color: $purple-500!important;
+      }
+      h4 {
+        flex: 1;
+        font-weight: 400;
+      }
+    }
+  }
   .tabs-container {
     display: flex;
     justify-content: flex-start;
@@ -332,13 +480,89 @@
         }
         .tab-item-content-body {
           display: flex;
+          flex-wrap: wrap;
 
+          ul.check-list {
+            flex-basis: 100%;
+            list-style: none;
+            padding: 0;
+            margin: 0;
+
+            li {
+              i {
+                color: $grey-500;
+              }
+            }
+            li.normal {
+              i {
+                color: $pink-262;
+              }
+            }
+            li.disabled {
+              i {
+                color: $grey-500;
+              }
+              span {
+                color: #c2c2c2;
+                text-decoration: line-through;
+              }
+            }
+          }
+          .notes {
+            background: #f9f9f9;
+            flex-basis: 100%;
+            border: 1;
+            border: 1px solid #e6e6e6;
+            margin-top: 2em;
+            padding: 1em 2em;
+
+            .notes-title {
+              h4 {
+                margin-top: 0;
+                font-weight: 600;
+                font-size: 16px;
+                margin-bottom: .5em;
+              }
+            }
+            .notes-body {
+              display: flex;
+              flex-direction: row;
+              flex-wrap: wrap;
+              flex-flow: row wrap;
+              
+              .note-item {
+                margin-right: 2em;
+                width: 40%;
+                color: $purple-500;
+                i {
+                  color: $purple-500;
+                }
+
+                @media (max-width: $screen-sm-min) {
+                  margin-right: 0;
+                  width: 100%;
+                }
+              }
+            }
+          }
+          .text-vertical {
+            width: 25%;
+            text-align: center;
+
+            h5 {
+              font-weight: 600;
+            }
+            @media (max-width: $screen-sm-min) {
+              margin-right: 0;
+              width: 50%;
+            }
+          }
           .icon-text-vertical {
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            padding: 0 2em;
+            padding-right: 2em;
 
             i {
               font-size: 36px!important;
@@ -348,6 +572,15 @@
               font-weight: 600;
               margin: 1em 0 5px 0;
             }
+
+            @media (max-width: $screen-sm-min) {
+              margin: 0;
+              padding: 0;
+              width: 33.33%;
+            }
+          }
+          a.more {
+            color: $purple-500;
           }
         }
       }
@@ -358,7 +591,7 @@
         margin: 2em 0;
         
         @media (max-width: $screen-sm-min) {
-          padding: 1em;
+          margin: 1em 0;
         }
       }
     }
