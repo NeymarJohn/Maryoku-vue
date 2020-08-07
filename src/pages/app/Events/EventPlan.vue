07ef39aeca42d627e7ad6ca5c0bd0e0a29e1c540
Sunriver <seriousman1994217winter@gmail.com>
2020-08-07 18:12:46 -0400

    updated requirement page

--- src/pages/app/Events/EventPlan.vue
+++ src/pages/app/Events/EventPlan.vue
@@ -4,7 +4,6 @@
     <event-details-timeline v-if="pageId=='timeline'"></event-details-timeline>
     <event-concept-choose v-else-if="pageId=='concept'"></event-concept-choose>
     <event-budget-requirement v-else-if="pageId=='budget'"></event-budget-requirement>
-    <event-campaign v-else-if="pageId='campaign'"></event-campaign>
     <booking-event v-else></booking-event>
   </div>
 </template>
@@ -14,7 +13,6 @@ import EventConceptChoose from './components/EventConceptChoose'
 import BookingEvent from './components/BookingEvent'
 import BookingEventRequirement from './components/BookingEventRequirement.vue'
 import EventBudgetRequirement from './components/EventBudgetRequirement.vue'
-import EventCampaign from '@/pages/app/Campaign/CampaignMainLayout.vue'
 import ProgressSidebar from "./components/progressSidebar";
 
 export default {
@@ -24,8 +22,7 @@ export default {
     BookingEvent,
     BookingEventRequirement,
     ProgressSidebar,
-    EventBudgetRequirement,
-    EventCampaign
+    EventBudgetRequirement
   },
   data() {
     return {
