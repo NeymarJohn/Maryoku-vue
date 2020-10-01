37577d0667bae7f4e90d70971ef051cf8d7a79fc
Sunriver <seriousman1994217winter@gmail.com>
2020-10-01 15:11:38 +0100

    add upload url

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,3 +1,4 @@
 export const ResourceURL = "https://static-maryoku.s3.amazonaws.com/";
 export const StorageURL = `https://static-maryoku.s3.amazonaws.com/storage/`;
 export const IconURL = `https://static-maryoku.s3.amazonaws.com/storage/icons/`;
+export const UploadURL = `https://maryoku.s3.amazonaws.com/`;
