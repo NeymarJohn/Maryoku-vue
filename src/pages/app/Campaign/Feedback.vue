b4212f90ff20bca436555d7e75dee3eb1b1c0f2c
Sunriver <seriousman1994217winter@gmail.com>
2020-11-05 19:05:10 +0900

    Fix the conflict

--- src/pages/app/Campaign/Feedback.vue
+++ src/pages/app/Campaign/Feedback.vue
@@ -101,7 +101,7 @@ export default {
   },
   created() {
     this.placeHolder = `Thank you so much for attending! We are so glad you could join us.
-    Please take a moment to help us improve future events by taking a brief survey. 
+    Please take a moment to help us improve future events by taking a brief survey.
     Your feedback is extremely valuable to our ongoing effort to offer great ${
       this.event.guestType || "employee"
     } experience.
