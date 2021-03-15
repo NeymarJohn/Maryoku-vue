ac89bc476f30b665bffbf5e894adcdedf609356f
Sunriver <seriousman1994217winter@gmail.com>
2021-03-15 09:33:01 -0400

    save notification to the DB

--- src/pages/app/Vendors/components/NotificationSettingItem.vue
+++ src/pages/app/Vendors/components/NotificationSettingItem.vue
@@ -4,7 +4,11 @@
     <div class="mt-10">{{ setting.description }}</div>
     <div v-for="option in setting.options" :key="option.name" class="d-flex justify-content-between align-center">
       <div>{{ option.name }}</div>
-      <md-switch class="md-switch-vendor large-switch" v-model="option.enabled"></md-switch>
+      <md-switch
+        class="md-switch-vendor large-switch"
+        v-model="option.enabled"
+        @change="changeOption(option.field, option.enabled)"
+      ></md-switch>
     </div>
   </div>
 </template>
@@ -14,6 +18,11 @@ export default {
     setting: {
       type: Object,
       default: () => {},
+    },
+  },
+  methods: {
+    changeOption(option, value) {
+      this.$emit("change", this.setting.settingName, option, value);
     },
   },
 };
