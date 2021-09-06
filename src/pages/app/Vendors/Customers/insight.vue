2ba19a16bf660aa900fa36268e1519133bacaf01
Sunriver <seriousman1994217winter@gmail.com>
2021-09-06 10:44:33 +0800

    fixed the issue in proposal and vendor offers page

--- src/pages/app/Vendors/Customers/insight.vue
+++ src/pages/app/Vendors/Customers/insight.vue
@@ -42,7 +42,7 @@
                   <span class="prev handle-btn" ref="prevButton"></span>
               </template>
               <div v-for="(p, index) in incomeList" :key="p.id" class="carousel-item">
-                  <template v-if="index == 1">
+                  <template v-if="p === 'tip'">
                       <div class="color-white font-size-20 font-bold-extra text-center mt-30">How to increase Success And Profit</div>
                       <div class="d-flex align-center w-max-400 mx-auto mt-20">
                           <img class="mr-20" :src="`${$iconURL}VendorsProposalPage/group-5280.svg`" style="width: 24px" />
@@ -72,7 +72,7 @@
                           >
                       </div>
                   </template>
-                  <template v-if="index == 0">
+                  <template v-if="p === 'graph'">
                       <h5 class="color-white font-size-20 font-bold-extra text-center">Income From Past And Future Events</h5>
                       <income-bar-chart :chartData="incomeChartData"></income-bar-chart>
                       <div class="md-layout my-40">
@@ -96,7 +96,33 @@
               </template>
           </carousel>
       </template>
-      <analytics v-else/>
+      <template v-else>
+          <carousel :items="1" :margin="0" :nav="false" :loop="true" class="d-flex">
+              <template slot="prev">
+                  <span class="prev handle-btn" ref="prevButton"></span>
+              </template>
+              <div v-for="(p, index) in incomeList" :key="p.id" class="carousel-item">
+                  <template v-if="p === 'temp'">
+                      <h5 class="color-white font-size-20 font-bold-extra text-center">Income From Past And Future Events</h5>
+                      <income-bar-chart :chartData="incomeChartData"></income-bar-chart>
+                      <div class="md-layout my-40">
+                          <div class="md-layout-item font-size-16 color-white md-size-50 text-left pl-50">2021
+                              <md-icon style="color: white">keyboard_arrow_down</md-icon>
+                          </div>
+                          <div class="md-layout-item md-size-50 d-flex align-center font-size-16 color-white text-right pr-50">
+                              <span :style="`background-color: #ffffff;`" class="icon mr-10"></span>
+                              No event incomes</div>
+                      </div>
+
+                      <hr :class="incomeList.length < 2 ? 'mb-40' : ''" />
+                  </template>
+              </div>
+              <template slot="next">
+          <span class="next handle-btn d-none" ref="nextButton">
+          </span>
+              </template>
+          </carousel>
+      </template>
     </div>
   </div>
 </template>
@@ -104,7 +130,6 @@
 import { PieChart } from "@/components";
 import IncomeBarChart from "./IncomeBarChart.vue";
 import ServiceBarChart from "./ServiceBarChart";
-import Analytics from "./Analytics";
 import carousel from "vue-owl-carousel";
 
 export default {
@@ -112,7 +137,6 @@ export default {
     PieChart,
     IncomeBarChart,
     ServiceBarChart,
-    Analytics,
     carousel,
   },
   props: {
@@ -142,8 +166,7 @@ export default {
         { label: "Nov", value: 60, future: true },
         { label: "Dec", value: 20, future: true },
       ],
-      incomeList: ["", ""],
-      renderCustomer: false,
+      incomeList: []
     };
   },
   mounted() {
@@ -152,6 +175,7 @@ export default {
   methods: {
     init() {
         console.log('customer.insight', this.aggregate, this.customer)
+        this.$forceUpdate();
         this.serviceChartData = this.customer ? [
             { label: "", value: 44, color: "#ffffff", icon: "Budget+Elements/venuerental-white.svg", price: "15000" },
             { label: "", value: 35, color: "#22cfe0", icon: "Budget+Elements/foodandbeverage-white.svg", price: "4800" },
@@ -161,6 +185,8 @@ export default {
             { label: "Catering", value: 35, color: "#915a87", icon: "Budget+Elements/foodandbeverage-white.svg", price: 0 },
             { label: "Signage & Printing", value: 20, color: "#4e0841", icon: "Budget+Elements/entertainment-white.svg", price: 0 },
         ];
+
+        this.incomeList = this.customer ? ["tip", "graph"] : ["temp"];
     },
     next() {
       this.$refs.nextButton.click();
