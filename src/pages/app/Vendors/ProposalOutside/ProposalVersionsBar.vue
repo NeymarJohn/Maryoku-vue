3efc7edc7b82aad3b7deb1e242c96e8fbd916f3e
Sunriver <seriousman1994217winter@gmail.com>
2021-09-28 15:21:12 +0800

    fix the field from 'event' while creating new version for non-maryoku proposal

--- src/pages/app/Vendors/ProposalOutside/ProposalVersionsBar.vue
+++ src/pages/app/Vendors/ProposalOutside/ProposalVersionsBar.vue
@@ -40,7 +40,7 @@ export default {
   data() {
     return {
         versionFields: [
-            'event',
+            'eventData',
             'costServices',
             'includedServices',
             'extraServices',
