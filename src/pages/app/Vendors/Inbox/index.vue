b63fc502931c1d48ea19728cda86efe1d05123ed
Sunriver <seriousman1994217winter@gmail.com>
2021-12-22 19:54:12 +0800

    add inbox page in vendor

---
+++ src/pages/app/Vendors/Inbox/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="event-plan">
+        <InboxSidebar />
+        <router-view></router-view>
+        hello
+    </div>
+</template>
+<script>
+const components = {
+    InboxSidebar: () => import('../components/InboxSidebar.vue'),
+}
+export default {
+    components,
+    data(){
+        return{}
+    }
+}
+</script>
---
+++ src/pages/app/Vendors/components/InboxSidebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="progress-sidebar">
+    <div class="summer-party">
+      <div class="title-label text-uppercase">
+        comment mode
+      </div>
+
+    </div>
+    <div class="progress-sidebar-content">
+      hey
+    </div>
+  </div>
+</template>
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+
+export default {
+  name: "inbox-sidebar",
+  components: {
+  },
+  props: {
+  },
+  data: () => ({
+  }),
+  computed: {
+  },
+
+  created() {
+  },
+  mounted() {},
+  watch: {
+  },
+};
+</script>
+<style lang="scss">
+</style>
