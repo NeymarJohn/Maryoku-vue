64dfc9a2e1f79ee32792562f42697e30620d8929
Sunriver <seriousman1994217winter@gmail.com>
2022-02-21 14:51:51 +0800

    update the API to dev server

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ var webpack = require('webpack')
 
 module.exports = merge(devEnv, {
     NODE_ENV: '"testing"',
-    // SERVER_URL: '"https://api-dev.maryoku.com"',
-    SERVER_URL: '"http://localhost:8080"',
+    SERVER_URL: '"https://api-dev.maryoku.com"',
+    // SERVER_URL: '"http://localhost:8080"',
     HOST_URL: '"dev.maryoku.com"',
     GOOGLE_API_KEY: '"AIzaSyD6JvMsiEN9jz3lfVbKlnUQhjZB6fgcCu8"',
 
