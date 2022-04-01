d3e672861c7ed60242459952774b7aef1fbeb42d
Sunriver <seriousman1994217winter@gmail.com>
2022-04-01 02:51:20 -0400

    update the discount policy section in the vendor signup page

--- src/pages/app/Vendors/Signup/VendorDiscountSection.vue
+++ src/pages/app/Vendors/Signup/VendorDiscountSection.vue
@@ -1,55 +1,35 @@
 <template>
   <div>
-    <div class="tabs-container md-layout mt-20 py-30 border-top">
+    <div class="tabs-container md-layout pb-30 border-top">
       <md-button v-for="option in tabOptions"
                  :key="option.value"
-                 class="md-simple md-outlined md-round md-vendor filter-button mr-10"
+                 class="md-round md-vendor filter-button mr-10"
+                 :class="tab === option.value ? '' : 'md-simple md-outlined'"
                  @click="changeTab(option.value)"
       >
         {{ option.label }}
       </md-button>
     </div>
-    <div class="fields-cont font-bold mb-10">
-      <span>Name</span>
-      <span class="text-left">Rules</span>
-      <span class="text-left">QTY</span>
-      <span class="font-size-14 text-left">Discount value / Percentage</span>
-    </div>
-    <hr style="background-color: #dddddd; height: 1px">
-    <div class="fields-cont mt-20">
-      <div class="field font-size-14">
-        If Number of Participants
+    <VendorDiscountAddForm
+      :tab="tab"
+      @add="addDiscountItem"
+    />
+    <template v-for="option in tabOptions">
+      <div v-if="discountData[option.value].length" :key="option.value" class="citems">
+        <div class="citem font-size-18 font-bold-extra mt-50 pb-10">
+          {{ option.label }}
+        </div>
+        <div class="citem cursor-pointer">
+          <vendor-discount-policy-item
+            v-for="(cs, csIndex) in discountData[option.value]"
+            :key="csIndex"
+            :tab="option.value"
+            :item="cs"
+            @change="changeDiscountItem(csIndex, $event)"
+          />
+        </div>
       </div>
-      <div class="field">
-        <money v-model="rule" class="text-center number-field" v-bind="rateFormat" />
-      </div>
-      <div class="field">
-        <input v-model="qty" type="number" class="text-center number-field" placeholder="100"/>
-      </div>
-      <div class="field">
-        <money v-model="unit" class="text-center number-field" v-bind="rateFormat" />
-
-        <md-button
-          class="md-vendor maryoku-btn ml-20"
-          :disabled="isDisabledAdd"
-        >
-          Add Line
-        </md-button>
-      </div>
-    </div>
-    <div v-for="option in tabOptions" :key="option.value" class="citems">
-      <div class="citem font-size-18 font-bold-extra mt-50 pb-10">
-        {{ option.label }}
-      </div>
-      <div class="citem cursor-pointer">
-        <vendor-discount-policy-item
-          v-for="(cs, csIndex) in getDiscountItems()"
-          :key="csIndex"
-          :item="cs"
-          @change="changeDiscountItem"
-        />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 <script>
@@ -58,61 +38,49 @@ import { DiscountTabOptions } from "@/constants/options";
 
 const components = {
     VendorDiscountPolicyItem: () => import("../components/vendor-discount-policy-item"),
+    VendorDiscountAddForm: () => import("../components/VendorDiscountAddForm"),
 };
 export default {
   components,
   props: {
-
+    data: {
+        type: Object,
+    }
   },
   data() {
       return {
-          rateFormat: {
-              decimal: ".",
-              thousands: ",",
-              prefix: "",
-              suffix: " %",
-              precision: 2,
-              masked: false,
-          },
         tab: "coupon",
-        rule: 0,
-        qty: 0,
-        unit: 0,
         tabOptions: DiscountTabOptions,
         discountData: {
-            coupon: [
-                {name: "Happyholiday22", sub : "Holidays 2022", value: "15%"},
-                {name: "Nice-to-meet", sub : "new customer", value: "7%"},
-            ],
-            number_of_guests: [
-                  {name: "Happyholiday22", sub : "100-9,999 Guests", value: "15%"},
-                  {name: "Nice-to-meet", sub : "1000 Guests Up", value: "7%"},
-            ],
-            customer_type: [
-                {name: "VIP customer", value: "7%"},
-                {name: "Regular customer", sub : "22 September - 21 December", value: "4%"},
-            ],
-            seasonal: [
-                {name: "Winter", sub : "21 December -20 March 2022", value: "7%"},
-                {name: "Above $4000", sub : "22 September - 21 December", value: "4%"},
-            ],
-        }
+            coupon: [],
+            number_of_guests: [],
+            customer_type: [],
+            seasonal: [],
+        },
+
       };
   },
-  computed: {
-    isDisabledAdd() {
-        return !this.qty || !this.unit;
-    },
-  },
   methods: {
+      init() {
+        Object.keys(this.discountData).forEach(key => {
+           if(this.data[key] && this.data[key].length)  this.discountData[key] = this.data[key];
+        });
+      },
       changeTab(value) {
           this.tab = value;
       },
-      getDiscountItems() {
-        return this.discountData[this.tab];
+      addDiscountItem(value){
+          this.discountData[this.tab].push(value);
+          this.$emit("save", this.discountData);
       },
-      changeDiscountItem() {
-
+      changeDiscountItem(index, {type, value}) {
+        console.log('change.item', index, type, value);
+        if (type === "update") {
+            this.discountData[this.tab][index] = value;
+        } else if (type === "remove") {
+            this.discountData[this.tab].splice(index, 1);
+        }
+        this.$emit("save", this.discountData);
       }
   },
 };
@@ -123,44 +91,14 @@ export default {
   border: 1px solid #641856;
   border-radius: 20px;
 }
-.fields-cont {
-    display: grid;
-    align-items: center;
-    grid-template-columns: 20% 20% 20% 40%; //
-    gap: 1%;
-    .field {
-        position: relative;
-        span {
-            margin-bottom: 0.5rem;
-            display: inline-block;
-            font: 800 16px "Manrope-Regular", sans-serif;
-        }
 
-        &:last-child {
-            margin-right: 0;
-        }
-    }
-}
 .citems {
-    .citem {
-        width: calc(100% - 5rem - 50px);
-        border-bottom: 1px solid #dddddd;
-    }
+    width: calc(100% - 7rem);
+    display: grid;
+    grid-column-gap: 3rem;
+    align-items: center;
 }
-input {
-    text-align: center;
-    font-size: 16px;
-    padding: 12px 20px;
-    border: 1px solid #dddddd;
-    border-radius: 0;
+.citem {
+    border-bottom: .5px solid #dddddd;
 }
-.number-field {
-    text-align: center;
-    font-size: 16px;
-    padding: 12px 20px;
-    width: 12rem;
-    border: 1px solid #dddddd;
-    border-radius: 0;
-}
-
 </style>
--- src/pages/app/Vendors/components/vendor-discount-policy-item.vue
+++ src/pages/app/Vendors/components/vendor-discount-policy-item.vue
@@ -1,32 +1,96 @@
 <template>
-  <div class="vendor-extra-pay-item-wrapper">
+  <div class="vendor-discount-item-wrapper">
     <div class="collapsed" @click="expand()">
-      <div class="col label">
-        {{ editingData.name }}
-      </div>
-      <div v-if="!isEditable" class="col">
-        <span v-if="editingData.sub">{{ editingData.sub }}</span>
-        <span v-else>-</span>
-      </div>
-      <div v-else class="col">
-        <div>
-          <input v-model="editingData.sub">
+      <template v-if="tab === 'coupon'">
+        <div v-if="!isEditable" class="col label">
+          {{ editingData.code }}
         </div>
-      </div>
-      <div v-if="!isEditable" class="col">
-        <span v-if="editingData.qty">{{editingData.qty }}</span>
-        <span v-else></span>
-      </div>
-      <div v-else class="col">
-        <div>
+        <div v-else class="col">
+            <input v-model="editingData.code" class="text-left number-field">
+        </div>
+
+        <div v-if="!isEditable" class="col">
+          <span v-if="editingData.validDate">{{ editingData.validDate }}</span>
+        </div>
+        <div v-else class="col">
+          <maryoku-input
+            v-model="editingData.validDate"
+            :value="editingData.validDate"
+            class="form-input width-90"
+            placeholder="Choose date…"
+            input-style="date"
+            @input="changeDate"
+          />
+        </div>
+      </template>
+      <template v-if="tab === 'number_of_guests'">
+        <div v-if="!isEditable" class="col text-left">
+          <span>{{ getRuleLabel(editingData.rule) }}</span>
+        </div>
+        <div v-else class="col">
+          <Multiselect
+            :key="rules.length"
+            v-model="editingData.rule"
+            class="width-90 mt-5 form-input md-purple"
+            :options="rules"
+            :close-on-select="true"
+            :clear-on-select="true"
+            tag-placeholder="Add this as new tag"
+            placeholder="Type to search category"
+            label="label"
+            track-by="value"
+          />
+        </div>
+
+        <div v-if="!isEditable" class="col">
+          <span v-if="editingData.qty">{{ editingData.qty | withComma(Number) }} Guests</span>
+        </div>
+        <div v-else class="col">
           <input v-model="editingData.qty">
         </div>
-      </div>
+      </template>
+
+      <template v-if="tab === 'customer_type'">
+        <div v-if="!isEditable" class="col text-left">
+          <span>{{ getCustomerTypeLabel(editingData.type) }} Customer</span>
+        </div>
+        <div v-else class="col">
+          <Multiselect
+            :key="rules.length"
+            v-model="editingData.type"
+            class="width-90 mt-5 form-input md-purple"
+            :options="rules"
+            :close-on-select="true"
+            :clear-on-select="true"
+            tag-placeholder="Add this as new tag"
+            placeholder="Type to search category"
+            label="label"
+            track-by="value"
+          />
+        </div>
+        <div class="col" />
+      </template>
+      <template v-if="tab === 'seasonal'">
+        <div v-if="!isEditable" class="col text-left">
+          {{ editingData.name }}
+        </div>
+        <input v-else v-model="editingData.code" class="col">
+
+        <div class="col text-capitalize">
+          {{ editingData.season }}
+          <template v-for="(month, idx) in editingData.months">
+            <span class="ml-10">{{ getMonthLabel(month) }}{{ idx < editingData.months.length - 1 ? ',' : '' }}</span>
+          </template>
+        </div>
+      </template>
       <div v-if="!isEditable" class="col text-right">
-        {{editingData.value}}
+        {{ `${editingData.value}${editingData.rate}` }}
       </div>
       <div v-else class="col">
-        <money v-model="editingData.value" class="text-center number-field" v-bind="rateFormat" />
+        <div class="position-relative">
+            <money v-model="editingData.value" class="text-center number-field" v-bind="rateFormat" />
+            <button class="suffix" @click="changeRate('seasonal')">{{editingData.rate}}</button>
+        </div>
       </div>
       <div v-if="!isEditable" class="col action">
         <img class="mr-20 ml-auto" :src="`${$iconURL}Requirements/edit-dark.svg`" @click="edit">
@@ -37,8 +101,7 @@
           Cancel
         </md-button>
         <md-button
-          class="md-red maryoku-btn"
-          :class="{ 'md-red': theme === 'red', 'md-vendor': theme === 'purple' }"
+          class="md-vendor maryoku-btn"
           @click="save"
         >
           Save
@@ -53,14 +116,21 @@
 </template>
 
 <script>
-import { Money } from "v-money";
+import { DiscountCustomerTypes, CouponRules, MonthOptions } from "@/constants/options";
+
+const components = {
+    MaryokuInput: () => import("@/components/inputs/MaryokuInput.vue"),
+    Multiselect: () => import("vue-multiselect"),
+};
+
 export default {
-    components: {
-        Money,
-    },
+    components,
     filters: {},
     props: {
         item: Object,
+        tab: {
+            type: String,
+        }
     },
     data() {
         return {
@@ -71,20 +141,35 @@ export default {
                 decimal: ".",
                 thousands: ",",
                 prefix: "",
-                suffix: " %",
                 precision: 2,
                 masked: false,
             },
+            monthOptions: MonthOptions,
+            rules: CouponRules,
+            customerTypes: DiscountCustomerTypes,
         };
     },
-    computed: {},
-    watch: {},
-    created() {},
     mounted() {
-        console.log("discount.item", this.item);
+        console.log("discount.item", this.item, this.tab);
         this.editingData = Object.assign({}, this.item);
     },
     methods: {
+        changeRate(tab){
+            this.editingData.rate = this.editingData.rate === "%" ? "$" : "%";
+        },
+        getMonthLabel(value) {
+            console.log('mon.val', value);
+            if (!value) return "";
+            return this.monthOptions.find(r => r.value == value).label;
+        },
+        getRuleLabel(value) {
+            if (!value) return "";
+            return this.rules.find(r => r.value === value).label;
+        },
+        getCustomerTypeLabel(value){
+            if (!value) return "";
+            return this.customerTypes.find(c => c.value === value).label;
+        },
         expand() {
             if (this.item.desc) {
                 this.expanded = !this.expanded;
@@ -103,16 +188,18 @@ export default {
         changePrice(value) {
             console.log(value);
         },
+        changeDate() {
+
+        },
         changeItem() {
-            this.$emit("change", this.editingData);
+            this.$emit("change", {type: 'update', value: this.editingData} );
         },
         edit() {
             this.isEditable = true;
+            console.log('editing', this.editingData);
         },
         remove() {
-            this.item.checked = false;
-            console.log("remvoe", this.item);
-            this.$emit("change", this.item);
+            this.$emit("change", {type: 'remove'});
         },
         cancel() {
             this.editingItem = Object.assign({}, this.item);
@@ -126,14 +213,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.vendor-extra-pay-item-wrapper {
+.vendor-discount-item-wrapper {
     font-family: Manrope-Regular, sans-serif;
     color: #050505;
     border-bottom: 1px solid #dddddd;
 
     .collapsed {
+        position: relative;
         display: grid;
-        grid-template-columns: 35% 35% 10% 10% 10%;
+        grid-template-columns: 30% 30% 30% 20%;
         padding: 1rem 0;
         grid-gap: 5px;
         .col {
@@ -141,6 +229,7 @@ export default {
             display: flex;
             align-items: center;
             padding: 10px 0;
+
             img {
                 width: 20px;
             }
@@ -154,28 +243,46 @@ export default {
 
             input {
                 text-align: center;
-                border-radius: 3px;
-                border: 1px solid #818080;
-                max-width: 160px;
-                font: normal 16px Manrope-Regular, sans-serif;
-                color: #050505;
+                font-size: 16px;
+                padding: 12px 20px;
+                border: 1px solid #dddddd;
+                border-radius: 0;
+                min-height: 55px;
             }
 
             .field {
                 display: inline-block;
             }
 
-            .field:before {
-                content: "$";
+            .number-field {
+                text-align: left;
+                font-size: 16px;
+                padding: 12px 20px;
+                border: 1px solid #dddddd;
+                border-radius: 0;
+                width: 90%;
+            }
+
+            button.suffix{
                 position: absolute;
-                margin-top: 15px;
-                margin-left: 3rem;
+                transform: translateX(-100%);
+                height: 100%;
+                width: 60px;
+                background: white;
+                border: 0.5px solid #dddddd;
+                font-size: 16px;
+                cursor: pointer;
             }
         }
 
         .action {
+            position: absolute;
+            right: -100px;
+            top: 0;
+            height: 100%;
             opacity: 0;
             display: flex;
+            align-items: center;
             flex-flow: nowrap;
             cursor: pointer;
         }
