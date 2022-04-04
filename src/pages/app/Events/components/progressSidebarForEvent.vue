d2d41ad9abb461fdcb01592159260881b64fde76
Sunriver <seriousman1994217winter@gmail.com>
2022-04-04 19:33:26 +0300

    Fix bug replacing logo on RSVP page

--- src/pages/app/Events/components/progressSidebarForEvent.vue
+++ src/pages/app/Events/components/progressSidebarForEvent.vue
@@ -1,80 +1,68 @@
 <template>
-  <div class="progress-sidebar">
-    <div class="summer-party">
-      <div v-if="page === 'plan'" class="title-label">
-        <div v-if="event.concept">
-          {{ event.concept.name }}
-        </div>
-        <div v-else>
-          Event Control Panel
-        </div>
-        <small>checklist</small>
-      </div>
-      <div v-if="page === 'event'" class="subTitle-label">
-        Let's Begin
-      </div>
-      <div class="completion-progress">
-        <div class="progress-done" />
-      </div>
-      <div class="percentage">
-        <ul>
-          <li class="green-label">
-            {{ (((warming / elements.length).toFixed(2) * 10000) / 100) | withComma }}%
-          </li>
-          <li class>
-            {{ warming }} of {{ elements.length }}
-          </li>
-        </ul>
-      </div>
-      <div v-if="page === 'plan'" class="small-label">
-        Things are warming up!
-      </div>
-      <div v-if="page === 'event'" class="small-label">
-        Only 4 more vendors to close!
-      </div>
-    </div>
-    <div class="progress-sidebar-content">
-      <!-- Sidebar Elements -->
-      <div class="event-elements">
-        <draggable :list="localElements" @change="changeItem">
-          <div
-            v-for="(item, index) in localElements"
-            :id="item.id"
-            :key="index"
-            class="event-elements__item"
-            :class="{
-              current: isActiveRoute(item),
-            }"
-            @click="goToRoute(item, index)"
-          >
-            <div class="item-title">
-              <img
-                v-if="item.status === 'completed' || item.progress === 100 || item.progress > 100"
-                :src="`${$iconURL}budget+screen/SVG/Asset%2032.svg`"
-                width="25"
-              >
-              <img
-                v-if="isActiveRoute(item) && item.icon"
-                :src="item.icon"
-                width="25"
-                style="max-width: 25px; max-height: 25px"
-              >
-              {{ item.title }}
-            </div>
-          </div>
-        </draggable>
-        <md-button
-          class="md-simple refresh"
-          :style="{
-            bottom: `${56 * (elements.length % 2 == 0 ? elements.length / 2 : (elements.length - 1) / 2 + 1)}px`,
-          }"
-          @click="changeItem('refresh')"
-        >
-          <img :src="`${$iconURL}Budget Requirements/group-9602@3x.png`" width="80">
-        </md-button>
-      </div>
-    </div>
-  </div>
+	<div class="progress-sidebar">
+		<div class="summer-party">
+			<div v-if="page === 'plan'" class="title-label">
+				<div v-if="event.concept.name">
+					{{ event.concept.name }}
+				</div>
+				<div v-else>Event Control Panel</div>
+				<small>checklist</small>
+			</div>
+			<div v-if="page === 'event'" class="subTitle-label">Let's Begin</div>
+			<div class="completion-progress">
+				<div class="progress-done" />
+			</div>
+			<div class="percentage">
+				<ul>
+					<li class="green-label">{{ (((warming / elements.length).toFixed(2) * 10000) / 100) | withComma }}%</li>
+					<li class>{{ warming }} of {{ elements.length }}</li>
+				</ul>
+			</div>
+			<div v-if="page === 'plan'" class="small-label">Things are warming up!</div>
+			<div v-if="page === 'event'" class="small-label">Only 4 more vendors to close!</div>
+		</div>
+		<div class="progress-sidebar-content">
+			<!-- Sidebar Elements -->
+			<div class="event-elements">
+				<draggable :list="localElements" @change="changeItem">
+					<div
+						v-for="(item, index) in localElements"
+						:id="item.id"
+						:key="index"
+						class="event-elements__item"
+						:class="{
+							current: isActiveRoute(item),
+						}"
+						@click="goToRoute(item, index)"
+					>
+						<div class="item-title">
+							<img
+								v-if="item.status === 'completed' || item.progress === 100 || item.progress > 100"
+								:src="`${$iconURL}budget+screen/SVG/Asset%2032.svg`"
+								width="25"
+							/>
+							<img
+								v-if="isActiveRoute(item) && item.icon"
+								:src="item.icon"
+								width="25"
+								style="max-width: 25px; max-height: 25px"
+							/>
+							{{ item.title }}
+						</div>
+					</div>
+				</draggable>
+				<md-button
+					class="md-simple refresh"
+					:style="{
+						bottom: `${56 * (elements.length % 2 == 0 ? elements.length / 2 : (elements.length - 1) / 2 + 1)}px`,
+					}"
+					@click="changeItem('refresh')"
+				>
+					<img :src="`${$iconURL}Budget Requirements/group-9602@3x.png`" width="80" />
+				</md-button>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
