7b1df480b2828a82c15ef5894e1f732f6247f519
Sunriver <seriousman1994217winter@gmail.com>
2022-04-18 21:46:37 +0300

    feedback page finished

--- src/store/modules/stripe.js
+++ src/store/modules/stripe.js
@@ -16,7 +16,7 @@ const getters = {
 
 const actions = {
   createDestinationAccount: ({commit, state}, data) => {
-    return postReq("/billing-information-test", data).then((res) => {
+    return postReq("/stripe/v1/customer/destinations/account", data).then((res) => {
       commit("setBankToken", res.data.token);
       return res.data.token;
     });
